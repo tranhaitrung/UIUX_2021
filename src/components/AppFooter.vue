b260093bb25976b5d388ef5f08a0ff46e4268da9
tranhaitrung <anhtrung.ttn@gmail.com>
2022-01-19 23:57:54 +0700

    thêm thông tin liên hệ

--- src/components/AppFooter.vue
+++ src/components/AppFooter.vue
@@ -5,6 +5,13 @@
         >Copyright by Công ty Cổ phần Công nghệ An toàn thông tin và Truyền
         thông Việt Nam.</span
       >
+      <br />
+      <span class="ms-1">Hotline: <a href="tel:0243012123">0243012123</a></span>
+      <br />
+      <span class="ms-1"
+        >Email to:
+        <a href="mailto:hotro@lichitech.xyz">hotro@lichitech.xyz</a>
+      </span>
     </div>
     <div class="ms-auto">
       <span class="me-1" target="_blank">Phiên bản 2019.1</span>
